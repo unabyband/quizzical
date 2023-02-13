cc09c041ad5196abd4cd7e69154908974a808afc
unabyband <110461462+unabyband@users.noreply.github.com>
2023-02-13 13:14:37 +0200

    Minor changes

--- src/Start.js
+++ src/Start.js
@@ -5,7 +5,7 @@ export default function Start(props) {
     return (
         <div className="startpage">
             <h1>Quizzical</h1>
-            <p>Solo project - for Learn React course</p>
+            <p>Choose number of quetions:</p>
             <input type="number" className="questionsNumber" placeholder="5" onChange={props.getNumber} min="1" max="99"/>
             <button onClick={props.onClick}>Start quiz</button>
         </div>
